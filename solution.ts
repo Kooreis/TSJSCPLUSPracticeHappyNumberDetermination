da1ac96a78b4bf54822d3d020e34b29502b258d1
Kooreis <kooreis.official@gmail.com>
2023-09-05 07:46:49 -0500

    This is the main function 'isHappy' which checks if a number is a 'happy' number. It uses a helper function 'sumOfSquares' to calculate the sum of squares of its digits. It also uses a 'seen' object to keep track of numbers already encountered in the process.

--- solution.ts
+++ solution.ts
@@ -1,4 +1,3 @@
-```typescript
 function isHappy(n: number): boolean {
     let seen: {[key: number]: boolean} = {};
     while (n !== 1 && !seen[n]) {
@@ -7,18 +6,3 @@ function isHappy(n: number): boolean {
     }
     return n === 1;
 }
-
-function sumOfSquares(n: number): number {
-    let sum = 0;
-    while (n > 0) {
-        let digit = n % 10;
-        sum += digit * digit;
-        n = Math.floor(n / 10);
-    }
-    return sum;
-}
-
-// Test the function
-console.log(isHappy(19));  // Output: true
-console.log(isHappy(2));   // Output: false
-```
