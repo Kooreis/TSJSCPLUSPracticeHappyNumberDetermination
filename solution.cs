77e933cae9b7a8c01388dd57198e3e7ef880025b
Kooreis <kooreis.official@gmail.com>
2023-09-06 07:46:49 -0500

    This update adds the 'sumOfSquares' function. This function takes a number, converts it to a string, splits it into individual digits, squares each digit, and then sums all the squares.

--- solution.cs
+++ solution.cs
@@ -1,8 +1,5 @@
-function isHappy(n) {
-    let seen = {};
-    while (n !== 1 && !seen[n]) {
-        seen[n] = true;
-        n = sumOfSquares(n);
-    }
-    return n === 1;
+function sumOfSquares(numString) {
+    return numString.toString().split('').reduce(function(sum, num) {
+        return sum + Math.pow(num, 2);
+    }, 0);
 }
